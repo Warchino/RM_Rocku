ad1cdfb2e2da27a21368076bafa0ef433f548aa7
Rodrigo Menacho <rmenacho21@gmail.com>
2019-02-23 13:44:15 -0400

    Fixed requests

--- vagrant/install-jenkins-dc.sh
+++ vagrant/install-jenkins-dc.sh
@@ -1,6 +1,3 @@
-# Pulling the container of jenkins
-docker pull registry.jala.info/devops/jenkins/jenkins:2.150.3-alpine
-
 # Creation of the parent dir for the jenkins data
 mkdir -p docker/jenkins_home
 chmod 777 docker/jenkins_home
